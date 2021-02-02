9fc72c10292830c91b6cd0f0aa9817ec394566e7
Hans Brinck <hans@bizzdo.se>
2021-02-02 15:51:03 +0100

    fixed: missing correct filename at 'init'

--- cmd-init.go
+++ cmd-init.go
@@ -18,10 +18,12 @@ func initVendor(name string) {
 	v = &Vendor{
 		Ignore:   "test",
 		RootPath: strings.TrimPrefix(wd, fmt.Sprintf("%s%c", filepath.Join(args.GoPath, "src"), filepath.Separator)),
+		filename: name,
 	}
 	if args.DryRun {
 		fmt.Printf("dry-run: would create '%s' with package = '%s'\n", name, v.RootPath)
 	} else {
+		os.MkdirAll(filepath.Dir(name), 0)
 		v.save()
 	}
 }
