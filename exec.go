d60324b27b4ae8120371b915359bc3c1713173a9
Hans Brinck <hans@bizzdo.se>
2021-02-04 17:49:39 +0100

    better error when no .go-files on 'deps'

--- exec.go
+++ exec.go
@@ -3,20 +3,30 @@ package main
 import (
 	"bufio"
 	"os/exec"
+	"strings"
 	"sync"
 )
+
+type execStdErr []string
+
+func (e execStdErr) Error() string {
+	return strings.Join([]string(e), ", ")
+}
 
 func execProc(folder, cmdname string, args ...string) ([]string, error) {
 	cmd := exec.Command(cmdname, args...)
 	cmd.Dir = folder
 
 	pipe, _ := cmd.StdoutPipe()
+	errpipe, _ := cmd.StderrPipe()
 
 	scan := bufio.NewScanner(pipe)
+	errscan := bufio.NewScanner(errpipe)
 	var wg sync.WaitGroup
 	var list []string
+	var errlist execStdErr
 
-	wg.Add(1)
+	wg.Add(2)
 	go func() {
 		for scan.Scan() {
 			line := scan.Text()
@@ -25,11 +35,26 @@ func execProc(folder, cmdname string, args ...string) ([]string, error) {
 		wg.Done()
 	}()
 
+	go func() {
+		for errscan.Scan() {
+			line := errscan.Text()
+			if len(line) > 0 {
+				errlist = append(errlist, line)
+			}
+		}
+		wg.Done()
+	}()
+
 	err := cmd.Run()
+	wg.Wait()
+
+	if len(errlist) > 0 {
+		return list, errlist
+	}
+
 	if err != nil {
 		return nil, err
 	}
-	wg.Wait()
 
 	return list, nil
 }
